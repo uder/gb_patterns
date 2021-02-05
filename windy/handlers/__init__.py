8c5100f3fcc32165dfab44714f990ab6feb70a1b
Salnikov <salnikov@100sp.ru>
2021-02-05 14:27:40 +1000

    Adds feedback page POST request procession

--- windy/handlers/__init__.py
+++ windy/handlers/__init__.py
@@ -10,6 +10,16 @@ def about(windy,environ,start_response,request):
 
 def feedback(windy,environ,start_response,request):
 	start_response(windy.http_200, windy.response_headers)
+	if request['method']=='POST':
+		name=request['data'].get('name',"Anonymous")
+		email=request['data'].get('email',"Without Mail")
+		subject=request['data'].get('subject','Without Subject')
+		message=request['data'].get('message','Empty Message')
+
+		print("---")
+		print(f'We have received Feedback from: "{name}" with return mail: "{email}" with subject: "{subject}" and message: "{message}"')
+		print("---")
+
 	html=windy.render('feedback.html', **request).encode('utf-8')
 	return [html]
 
--- windy/middleware/get_request_data.py
+++ windy/middleware/get_request_data.py
@@ -1,9 +1,27 @@
 from urllib.parse import urlparse, parse_qs
 
+def _parse_request_string(string):
+    data={}
+    parse_dict=parse_qs(string)
+    for key in parse_dict:
+        data.update({key:parse_dict[key][0]})
+
+    return data
+
+
 def get_request_data(windy,request,environ):
     request['method'] = environ.get('REQUEST_METHOD')
+    data_str=""
     if environ.get('REQUEST_METHOD')=="GET":
-        request['data']=parse_qs(environ.get('QUERY_STRING'))
+        data_str=environ.get('QUERY_STRING',"")
+    elif environ.get('REQUEST_METHOD')=="POST":
+        content_length=int(environ.get('CONTENT_LENGTH','0'))
+        if content_length>0:
+            data_str=environ['wsgi.input'].read(content_length).decode('utf-8')
+        else:
+            data_str=""
+
+    request['data']=_parse_request_string(data_str)
 
     return request
 
