f840a901030ae435d436f2ae2f3248907bcecd31
Salnikov <salnikov@100sp.ru>
2021-02-05 13:45:44 +1000

    Adds feedback page

--- windy/handlers/__init__.py
+++ windy/handlers/__init__.py
@@ -1,15 +1,18 @@
 def root(windy,environ,start_response,request):
-	author=request.get('author',None)
 	start_response(windy.http_200, windy.response_headers)
-	html=windy.render('index.html', author=author).encode('utf-8')
+	html=windy.render('index.html', **request).encode('utf-8')
 	return [html]
 	
 def about(windy,environ,start_response,request):
-	author=request.get('author',None)
 	start_response(windy.http_200, windy.response_headers)
-	html=windy.render('about.html', author=author).encode('utf-8')
+	html=windy.render('about.html', **request).encode('utf-8')
 	return [html]
-	
+
+def feedback(windy,environ,start_response,request):
+	start_response(windy.http_200, windy.response_headers)
+	html=windy.render('feedback.html', **request).encode('utf-8')
+	return [html]
+
 def not_found(windy,environ,start_response,request):
 	start_response(windy.http_404, windy.response_headers)
 	return [b'NOT EXISTENT PAGE '+environ['PATH_INFO'].encode('utf-8')]
