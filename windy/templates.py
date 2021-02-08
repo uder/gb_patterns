78929df9f146e15282576d86e203f58ff871d76e
Salnikov <salnikov@100sp.ru>
2021-02-08 14:41:08 +1000

    Adds template and subtemplates rendering

--- windy/templates.py
+++ windy/templates.py
@@ -1,4 +1,4 @@
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 import os
 
 
@@ -11,8 +11,11 @@ def render(template_name, folder='templates', **kwargs):
     """
     file_path = os.path.join(folder, template_name)
     # Открываем шаблон по имени
-    with open(file_path, encoding='utf-8') as f:
-        # Читаем
-        template = Template(f.read())
+    env=Environment()
+    env.loader=FileSystemLoader(folder)
+    template=env.get_template(template_name)
+#    with open(file_path, encoding='utf-8') as f:
+##        # Читаем
+ #       template = Template(f.read())
     # рендерим шаблон с параметрами
     return template.render(**kwargs)
