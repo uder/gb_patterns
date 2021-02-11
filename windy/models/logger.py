219258ced4496ad203a02137050ae508a8629f6d
Salnikov <salnikov@100sp.ru>
2021-02-11 16:24:49 +1000

    Adds WindyLogger class

---
+++ windy/models/logger.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime
+from windy.include_patterns.singleton import Singleton
+
+class WindyLogger(metaclass=Singleton):
+    def __init__(self,dir='logs'):
+        self.delimiter="---"
+        self.eol="\n"
+        self.dir=dir
+
+    def log(self,severity,target,message):
+        date=datetime.now()
+        record=f'{date} {severity}: {message}'
+        self.log_to_file(target,record)
+        self.log_to_console(record)
+
+    def log_to_file(self,target,record):
+        filename=target+'.log'
+        path=os.path.join(self.dir, filename)
+        with open(path,'a') as f:
+            f.write(record+self.eol)
+
+    def log_to_console(self,record):
+        print(self.delimiter)
+        print(record)
+        print(self.delimiter)
